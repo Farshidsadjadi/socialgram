3db1438f5672ce39b983692c053272f1b7763c4c
F.sadjadi <sadjadi.farshid@gmail.com>
2017-02-14 16:36:45 +0330

    pak kardane file haye ezafe

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from form import RegistrationForm
+from form import RegistrationForm, LoginForm
 from models import *
 
 app = Flask(__name__)
@@ -14,6 +14,11 @@ def register():
         new_user.save()
         return "success"
     return render_template("register.html", form_register=form)
+
+@app.route("/login", methods=['POST', 'GET'])
+@def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
 
 
 
